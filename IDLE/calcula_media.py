4e3c062c48d31759f2330a513b2e0dcda78a4c1a
devbelasp <isa.bela360@hotmail.com>
2025-03-19 16:57:37 -0300

    Exercício de fluxograma e README adicionados

---
+++ IDLE/calcula_media.py
@@ -0,0 +1,14 @@
+print('------------------------')
+print('*-* calculo da Média *-*')
+print('------------------------')
+
+#entrada de dados
+n1 = float(input('N1: '))
+n2 = float(input('N2: '))
+n3 = float(input('N3: '))
+
+#processamento - calculo da média
+media = (n1+n2+n3)/3
+
+#saída de dados
+print('Média: ',media)
